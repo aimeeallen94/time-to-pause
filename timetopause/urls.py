32885d906dcc51e1e648a81c35c59bf3c3505558
Aimee Allen <aimeeallen94@gmail.com>
2024-01-24 16:37:19 +0000

    install required django version for project

--- timetopause/urls.py
+++ timetopause/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('blog/', views.blog, name='blog'),
-    path('base/', views.base, name='base'),
+    path('base', views.base, name='base'),
     path('PostList/', views.PostList.as_view(), name='post_list'),
 ]
