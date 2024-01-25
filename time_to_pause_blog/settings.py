c8490e136375e2bb6063335647c3873626381714
Aimee Allen <aimeeallen94@gmail.com>
2024-01-25 17:53:41 +0000

    use json and fixtures inserted sample blog posts, connected static files to enable serving

--- time_to_pause_blog/settings.py
+++ time_to_pause_blog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-aimeeallen9-timetopause-r5xakyy5lv6.ws-eu107.gitpod.io', 'https://timetopause-4c93f0a77919.herokuapp.com/']
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
