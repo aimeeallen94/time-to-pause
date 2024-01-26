74ff33f5dd377f5342f75fa484cb5c680eb74473
Aimee Allen <aimeeallen94@gmail.com>
2024-01-26 17:52:18 +0000

    connect about model and view to display url on webpage app

--- about/models.py
+++ about/models.py
@@ -1,15 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
 # Create your models here.
 
 class About(models.Model):
     name = models.CharField(max_length=200)
-    favourite_quote = models.TextField()
-    my_story = models.TextField()
-    why_i_started = models.TextField()
+    favourite_quote = models.TextField(default='empty')
+    my_story = models.TextField(default='empty')
+    why_i_started = models.TextField(default='empty')
     date_created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return {self.name}
+        return self.name
--- about/views.py
+++ about/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.shortcuts import render
 from .models import About
 
 # Create your views here.
 
 
 def about_me(request):
-    about = About.objects.all().order_by('-created_on').first()
-    return render(request, "about/about.html", {"about":about},)
+    about = About.objects.all().order_by('-date_created').first()
+
+    return render(request, "about.html", {"about":about},)
