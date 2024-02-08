42329ba3ce0f41db2640f55e669333a1faa84478
Aimee Allen <aimeeallen94@gmail.com>
2024-02-08 20:23:47 +0000

    add like functionality to blog posts

--- timetopause/urls.py
+++ timetopause/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('post_list/', views.PostList.as_view(), name='post_list'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
 ]
