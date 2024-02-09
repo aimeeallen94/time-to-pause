f5a4b6d4f8e9e7accaa9a002bc3a44742c703698
Aimee Allen <aimeeallen94@gmail.com>
2024-02-09 15:28:36 +0000

    set up cloudinary storage of images

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 os.environ.setdefault("DATABASE_URL", "postgres://srxicomp:ZXklZIa7nVG3OGYMGWpbALJyIks-zSsj@snuffleupagus.db.elephantsql.com/srxicomp")
 os.environ.setdefault("SECRET_KEY", "AimeeAllen1994")
-os.environ["CLOUDINARY_URL"] = "cloudinary://864423287522164:3Vqp96yJnP6mQGTBW618OFCsMik@dykvpzxi9"
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://864423287522164:3Vqp96yJnP6mQGTBW618OFCsMik@dykvpzxi9")
